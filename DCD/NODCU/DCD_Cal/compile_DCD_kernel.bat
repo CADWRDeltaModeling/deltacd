4831db4bf06e97f7d88ad9a8a62387307397390b
Nicky Sandhu <psandhu@water.ca.gov>
2021-06-22 15:22:57 -0700

    adding 64bit compile and windows binary

--- DCD/NODCU/DCD_Cal/compile_DCD_kernel.bat
+++ DCD/NODCU/DCD_Cal/compile_DCD_kernel.bat
@@ -1,3 +1,3 @@
-"c:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" ia32 vs2015
+"c:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\bin\compilervars.bat" intel64 vs2015
 rem Improved to use buffered IO - NS
-ifort /O3 /assume:protect_parens,buffered_io,bytrerecl,minus0 -o DCD_kernel DCD_kernel.f
+ifort /O3 /assume:protect_parens,buffered_io,byterecl,minus0 -o DCD_kernel DCD_kernel.f
